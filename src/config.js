0b52ab89d71d1bfd2a2bf1423f18e8407799e54d
Berk Özdemir <berk.ozd@gmail.com>
2022-07-17 13:56:13 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const apiUrl = "https://speedy-nodes-nyc.moralis.io/9542c5e3fa5118d1f0de3f16/eth/mainnet"
+// export const apiUrl = "https://speedy-nodes-nyc.moralis.io/9542c5e3fa5118d1f0de3f16/eth/mainnet"
+export const apiUrl = "https://mainnet.infura.io/v3/4d6d6270fa6b4b87ae2b329280e7fbf7"
 
 export const BATCHER_ABI = [{"inputs":[{"internalType":"address","name":"erc721Address","type":"address"},{"internalType":"address","name":"user","type":"address"}],"name":"getIds","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"erc721Address","type":"address"},{"internalType":"address","name":"user","type":"address"}],"name":"getURIs","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"}]
 
